4af8832f8073ea85d14b5dd7b6551e49c53aefa7
ky-hwa <dlrudghk1301@naver.com>
2023-06-30 18:36:08 +0900

    django course 39 commit

--- projectapp/views.py
+++ projectapp/views.py
@@ -8,6 +8,7 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreateForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
 
 
 # Create your views here.
@@ -25,16 +26,25 @@ class ProjectCreateView(CreateView):
 class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
-    template_name = 'Projectapp/detail.html'
+    template_name = 'projectapp/detail.html'
 
     pagination_by = 25
 
     def get_context_data(self, **kwargs):
-        object_list = Article.objects.filter(object=self.get_object())
-        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
+        project = self.object
+        user = self.request.user
+
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project)
+
+        object_list = Article.objects.filter(project=self.get_object())
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list,
+                                                               subscription=subscription,
+                                                               **kwargs)
 
 class ProjectListView(ListView):
     model = Project
-    context_object_name = 'Project_list'
-    template_name = 'Projectapp/list.html'
+    context_object_name = 'project_list'
+    template_name = 'projectapp/list.html'
     paginate_by = 5
